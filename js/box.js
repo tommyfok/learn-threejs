71f19b60ff172ea61dfd588c60f20f82c1c069c3
tommyfok <154274174@qq.com>
2016-05-29 17:55:18 +0800

    update box.js

--- js/box.js
+++ js/box.js
@@ -1,6 +1,8 @@
+// 场景相关
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 10000);
 var controls  = new THREE.OrbitControls(camera);
+
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setClearColor(0xDDDDDD);
@@ -20,40 +22,45 @@ camera.lookAt({
 });
 
 // 添加灯光
-// var ambientLight = new THREE.AmbientLight('#ffffff');
-var pointLight1 = new THREE.SpotLight(0xFFFFFF);
-pointLight1.position.set(-50, 50, 50);
-pointLight1.castShadow = true;
-scene.add(pointLight1);
-// scene.add(ambientLight);
-var hemiLight = new THREE.HemisphereLight('#fff', '#666', 1);
-hemiLight.position.set(100, 500, 0);
-// hemiLight.castShadow = true;
-scene.add(hemiLight);
+var spotLight = new THREE.SpotLight(0xFFFFFF);
+spotLight.position.set(-50, 50, 50);
+spotLight.castShadow = true;
+spotLight.intensity = 4;
+scene.add(spotLight);
 
-// 添加一个几何体
-var cubeGeo = new THREE.CubeGeometry(10, 10, 10);
-// 创建橙色无高光材质
-var cubeMat = new THREE.MeshLambertMaterial({color: '#ff6600'});
-// 用几何体和材质生成网格（可加入场景了）
-var cube = new THREE.Mesh(cubeGeo, cubeMat);
-cube.position.y = 5;
-cube.castShadow = true;
-scene.add(cube);
+var sceneHandler = {
+  // 添加一个几何体
+  addCube: function () {
+    var size = getRandomInt(3);
+    var cubeGeo = new THREE.BoxGeometry(size, size, size, size, size, size);
+    var cubeMats = [
+      new THREE.MeshPhongMaterial({color: 0xFF6600, opacity: .6, transparent: true}),
+      new THREE.MeshLambertMaterial({color: 0x000000, wireframe: true})
+    ];
+    var cube = THREE.SceneUtils.createMultiMaterialObject(cubeGeo, cubeMats);
+    cube.position.x = Math.random() * 10;
+    cube.position.y = Math.random() * 10;
+    cube.position.z = Math.random() * 10;
+    // 实际上是两个mesh，所以foreach一下
+    cube.children.forEach(function (elem) {
+      elem.castShadow = true;
+      elem.receiveShadow = true;
+    });
+    scene.add(cube);
+  }
+};
 
 var gui = new dat.GUI();
-gui.add(cube.position, 'x', 0, 10);
-gui.add(cube.position, 'y', 0, 10);
-gui.add(cube.position, 'z', 0, 10);
+gui.add(sceneHandler, 'addCube');
 
-// 接下来创建一个平面
-var planeGeo = new THREE.PlaneGeometry(100, 100);
+// 创建一个地面
+var planeSize = 100;
+var planeGeo = new THREE.PlaneGeometry(planeSize,planeSize,planeSize,planeSize);
 var planeMat = new THREE.MeshLambertMaterial({color: '#666'});
 var plane = new THREE.Mesh(planeGeo, planeMat);
 plane.rotation.x = -.5*Math.PI;
 plane.receiveShadow = true;
 scene.add(plane);
-
 
 // 循环渲染场景函数
 function render () {
@@ -63,3 +70,7 @@ function render () {
 
 // 渲染场景
 render();
+
+function getRandomInt (range) {
+  return Math.floor(range*Math.random()) + 1;
+}
